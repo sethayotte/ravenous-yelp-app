6fde24a0a96d36d40d09a2124c2e4f3a4de063d0
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-09-14 20:39:42 -0400

    Initial App - Hard Coded Elements

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+  );
+}
+
+export default App;
